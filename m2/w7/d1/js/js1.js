f979197c829d7cccd8b7f855ca66ef5cbae2af48
RobiCavalli <roberta.cavalli94@gmail.com>
2022-12-05 16:53:50 +0100

    commit

---
+++ m2/w7/d1/js/js1.js
@@ -0,0 +1,34 @@
+// ossatura costruttore
+
+function utente (){
+    this.nome= "";
+    this.cognome = "";
+    this.dataNascita= "";
+}
+
+let user = new utente();
+let nome = document.querySelector("#name");
+let cognome = document.querySelector("#surname");
+let data = document.querySelector("#data");
+let bottone= document.querySelector('#invio_btn')
+ console.log(nome.value)
+
+ bottone.addEventListener('click', invio)
+
+// riempio il costruttore 
+
+let cella3 = document.querySelector('#cella3')
+let oggi = new Date ()
+
+bottone.addEventListener('click', invio)
+
+function invio (){
+ utente.nome =  nome.value
+ utente.cognome = cognome.value
+ utente.dataNascita = data.value
+cella.innerHTML = utente.nome
+cella2.innerHTML = utente.cognome
+cella3.innerHTML = utente.compleanno
+
+
+}
