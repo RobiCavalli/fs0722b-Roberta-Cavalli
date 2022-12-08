7c01c4ec04dd6a5da7ea0c56f71f2d534c9018da
RobiCavalli <roberta.cavalli94@gmail.com>
2022-12-08 12:41:57 +0100

    commit

--- m2/w7/d3/js/js.js
+++ m2/w7/d3/js/js.js
@@ -5,25 +5,30 @@ function delayed (){
         }, 3000);
     });
     }
-
-async function tempoChePassa (){
-    let fine = await delayed();
-    console.log(fine)
+    
+    
+     async function tempoChePassa (){
+    /*let fine = await delayed();
+    console.log(fine)*/
     
     
     if(typeof(Storage) != undefined){
         if(sessionStorage.clickcount){
     
-         sessionStorage.clickcount = setInterval(myTimer, 1000) ;
+         sessionStorage.clickcount = setInterval(myTimer, ) ;
         }else{
             sessionStorage.clickcount = 1
         }
         function myTimer() {
         const d = new Date();
-        document.getElementById("tempo").innerHTML = d.toLocaleTimeString();
+     
+        
+        document.getElementById("tempo").innerHTML ='tempo passato sul sito '  + d.getSeconds()
       }
     }
     
     
     }
-    tempoChePassa();
+    tempoChePassa()
+      
+        
