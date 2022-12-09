c6fb053408c6d2565985ffe1d4e78ca77fa7dd1b
RobiCavalli <roberta.cavalli94@gmail.com>
2022-12-09 15:09:56 +0100

    commit

---
+++ m2/w7/d5/js/js1.js
@@ -0,0 +1,19 @@
+fetch ('users.json').then(function (response){
+    return response.json();
+
+}).then(function(json){
+document.querySelector('#john1').innerHTML = classe[0].firstName + " " + classe[0].lastName + " " ;
+document.querySelector('#john_email').innerHTML = classe[0].email;
+document.querySelector('#jane1').innerHTML = classe[1].firstName + " " + classe[1].lastName;
+document.querySelector('#jane_email').innerHTML = classe[1].email;
+document.querySelector('#john-img').innerHTML = `<img class="bg-light border rounded-circle" src ="img/male.png"></img>`
+document.querySelector('#jane-img').innerHTML = `<img class="bg-light border rounded-circle" src ="img/female.png"></img>`
+}).catch(function(err){
+    console.log ('fetch problem: '+ err.message);
+
+});
+
+
+
+
+
