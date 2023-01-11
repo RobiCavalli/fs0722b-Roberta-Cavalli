7c2739c747e17cd554945a605c6203815220fb7c
RobiCavalli <roberta.cavalli94@gmail.com>
2023-01-11 16:23:18 +0100

    commit

--- m3/w9/d1/Esercizio1/src/app/app.module.ts
+++ m3/w9/d1/Esercizio1/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { PrimoComponentComponent } from './primo-component/primo-component.component';
 import { SecondoComponentComponent } from './secondo-component/secondo-component.component';
 import { TerzoComponentComponent } from './terzo-component/terzo-component.component';
+import { ProvaComponent } from './components/prova/prova.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimoComponentComponent,
     SecondoComponentComponent,
-    TerzoComponentComponent
+    TerzoComponentComponent,
+    ProvaComponent
   ],
   imports: [
     BrowserModule
