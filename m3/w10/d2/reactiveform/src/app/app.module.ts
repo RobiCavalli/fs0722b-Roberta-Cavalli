99a11fd35583f1dfa2bd0ab4ea64a3b5eed77e4c
RobiCavalli <roberta.cavalli94@gmail.com>
2023-01-17 17:15:45 +0100

    commit

---
+++ m3/w10/d2/reactiveform/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'
+
+import { AppComponent } from './app.component';
+import { ReactiveComponent } from './component/reactive/reactive.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReactiveComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
