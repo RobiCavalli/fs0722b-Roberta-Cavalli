d5b1874fd39df07b08d6d97f3a7a9781a4e7733b
RobiCavalli <roberta.cavalli94@gmail.com>
2023-02-01 16:55:30 +0100

    commit

---
+++ back-end/m1/w1/d3/EsercizioDay3/src/Esercizio1/Main.java
@@ -0,0 +1,71 @@
+package Esercizio1;
+
+import java.util.Scanner;
+
+public class Main {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pariDispari("ciao");
+		annoBisestile(2018);
+		contoRovescia(5);
+	}
+
+	public static void pariDispari(String a) {
+		if ((a.length() % 2) == 0) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+
+	}
+
+	public static void annoBisestile(int a) {
+		if (a % 4 == 0 && a % 100 != 0) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+
+	}
+	
+	
+	//2s
+		public static void stampaNumero(int n) {
+	        switch(n) {
+	        case 1: 
+	            System.out.println("uno");
+	            break;
+	        case 2:
+	            System.out.println("due");
+	            break;
+	        case 3:
+	            System.out.println("tre");
+	            break;
+	        default:
+	            System.out.println("ERRORE");
+	        }
+	    }
+		
+		// 3s
+		
+		/*public static void es3 (String a, String b ) {
+			while ( b != "q") {
+				System.out.println(a);
+				break;
+			}
+			
+		}*/
+		
+		//4s
+		
+		public static void contoRovescia(int a) {
+			 for (int i=a; i>=0; i=i-1)
+				System.out.println(i);
+			}
+	}
+	
+	
+	
+	
+
